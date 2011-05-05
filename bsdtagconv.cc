d18f808ca9e0dd78b06f17682cdf1a660cf7154d
Buganini Q <buganini@gmail.com>
2011-05-06 06:49:46 +0800

    put the theoretical code in comment

--- bsdtagconv.cc
+++ bsdtagconv.cc
@@ -451,6 +451,7 @@ int proc(char *file){
 			printUniTag(f.U_Tag);
 		}
 		if(testarg==0){
+			//f.strip(stripmask);
 			/* XXX taglib works weirdly here,
 			 * strip everthing w/o id3v2 is not working
 			 * but set ID3v2, strip all, then save
